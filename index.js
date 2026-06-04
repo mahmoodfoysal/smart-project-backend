a26f3469bd05ed719a51876165bb2e1553e79334
Foysal Mahmood <foysalcse033@gmail.com>
2026-06-04 14:23:40 +0600

    update dns

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ require("dotenv").config();
 const userRoute = require("./collections/user-collection.js");
 const projectManagementRoute = require("./collections/project-management-collection.js");
 const port = process.env.PORT || 5000;
-const dns = require("dns");
+// const dns = require("dns");
 
 app.use(cors());
 app.use(express.json());
 
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.da6po2r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
